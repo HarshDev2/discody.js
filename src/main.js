084830beccbec985dc2128fa4aa1aefb2dda3106
Harsh Agrawal <averageworkspace@gmail.com>
2023-06-27 09:24:09 +0530

    Updated Exports

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { Bot } from "./core/bot.js";
 import { EmbedBuilder } from "./core/embed_builder.js"
+import { Intents } from "./core/intents.js";
+import { SlashCommandBuilder } from "./core/slashCommandBuilder.js";
+import { Colors } from "./core/colors.js";
+import { Events } from "./core/events.js";
 
-export { Bot, EmbedBuilder };
+export { Bot, EmbedBuilder, Intents, SlashCommandBuilder, Colors, Events };
