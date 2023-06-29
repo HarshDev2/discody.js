92fcc3b2326f30fd19466b4d282c695d30762709
Harsh Agrawal <averageworkspace@gmail.com>
2023-06-29 09:48:25 +0530

    Removed Debug Code

--- src/utils/sendInteractionEditReply.js
+++ src/utils/sendInteractionEditReply.js
@@ -2,12 +2,6 @@ import axios from 'axios';
 
 export async function sendInteractionEditReply(bot, interaction, replyMessage, options) {
   if(!options) options = {};
-  console.log({
-    content: replyMessage,
-    embeds: options.embeds ?
-    options.embeds
-    : []
-  })
       const response = await axios.post(`https://discord.com/api/v10/webhooks/${bot.id}/${interaction.token}`, {
         content: replyMessage,
         embeds: options.embeds ? 
